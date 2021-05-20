900559040232e43d540cffef6f62d9f1300d2d10
anthibo <47614266+anthibo@users.noreply.github.com>
2021-05-21 00:49:49 +0200

    Creating all activities and testing changing activities using intents in java and kotlin

--- app/src/main/java/com/example/shopping_app/RegisterActivity.kt
+++ app/src/main/java/com/example/shopping_app/RegisterActivity.kt
@@ -1,11 +1,23 @@
 package com.example.shopping_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+
+//todo Activity functionality to be implemented
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val button:Button = findViewById<Button>(R.id.register_btn)
+        button.setOnClickListener {
+            moveToItemsActivity()
+        }
+    }
+    private fun moveToItemsActivity(){
+        val intent = Intent(this, ItemsActivity::class.java)
+        startActivity(intent)
     }
 }
