ee053d8af7ed7121811ea509322d65c0ac906199
anthibo <47614266+anthibo@users.noreply.github.com>
2021-05-21 00:04:57 +0200

    Moving from MainActivity to RegisterActivity

--- app/src/main/java/com/example/shopping_app/MainActivity.java
+++ app/src/main/java/com/example/shopping_app/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.shopping_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //first check if user is already registered or not
+        //todo first check if user is already registered or not
         //if user is not registered => load activity_main_unregistered layout
-        //else load activity_main_registered
+        //else load activity_main_registered //
         setContentView(R.layout.activity_main_unregistered);
+        // to be handled when applying the todo
+        Button home_btn = findViewById(R.id.home_btn);
+        home_btn.setOnClickListener(view -> moveToRegisterActivity());
+
+    }
+    public void moveToRegisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+    //todo to be implemented.
+    public void moveToItemsActivity(){
 
     }
 }
